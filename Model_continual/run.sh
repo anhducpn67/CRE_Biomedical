ca098f38524e9a2d654e4e5b882bb88b32205b4b
Nguyen Tran Anh Duc <anhducpn67@gmail.com>
2023-03-09 16:15:07 +0700

    Add file from incremental to continual

---
+++ Model_continual/run.sh
@@ -0,0 +1,14 @@
+
+
+# single head
+#  nohup python run.py --ID 7777 --GPU 0 --Training_way "Multi_Task_continual_Training" --Group_num 40 --Corpus_list "Twi_ADE" "ADE" "DDI" "CPR"  --All_data  >/dev/null 2>&1 &
+
+# multi head
+  #Multi-task continual
+#  nohup python run.py --ID 7777 --GPU 1 --Training_way "Multi_Task_continual_Training" --Group_num 40 --Corpus_list "DDI" "Twi_ADE" "ADE"  --All_data  >/dev/null 2>&1 &
+#  nohup python run.py --ID 7777 --GPU 3 --Training_way "Multi_Task_continual_Training" --Group_num 40 --Corpus_list "Twi_ADE" "ADE" "DDI" "CPR"  --All_data  >/dev/null 2>&1 &
+  # Continual
+#  nohup python run.py --ID 7777 --GPU 1 --Training_way "Multi_Task_continual_Training" --Group_num 1 --Corpus_list "DDI" "CPR" "Twi_ADE" "ADE"  --All_data  >/dev/null 2>&1 &
+
+
+python run.py --ID 7777 --GPU 0 --Training_way "Multi_Task_Training" --Entity_Prep_Way "entitiy_type_marker" --Group_num 1 --Corpus_list "Twi_ADE" "ADE" "DDI" "CPR" "PPI" --All_data
