48157cd141d48f452fc72b846dd6fa970320a3fb
Nguyen Tran Anh Duc <anhducpn67@gmail.com>
2023-03-22 16:28:17 +0700

    Add run_with_memory_samples.py

--- Model_continual/run.sh
+++ Model_continual/run.sh
@@ -1,13 +1,9 @@
 
 
-# single head
-#  nohup python run.py --ID 7777 --GPU 0 --Training_way "Multi_Task_continual_Training" --Group_num 40 --Corpus_list "Twi_ADE" "ADE" "DDI" "CPR"  --All_data  >/dev/null 2>&1 &
-
-# multi head
-  #Multi-task continual
-#  nohup python run.py --ID 7777 --GPU 1 --Training_way "Multi_Task_continual_Training" --Group_num 40 --Corpus_list "DDI" "Twi_ADE" "ADE"  --All_data  >/dev/null 2>&1 &
-#  nohup python run.py --ID 7777 --GPU 3 --Training_way "Multi_Task_continual_Training" --Group_num 40 --Corpus_list "Twi_ADE" "ADE" "DDI" "CPR"  --All_data  >/dev/null 2>&1 &
-  # Continual
-#  nohup python run.py --ID 7777 --GPU 1 --Training_way "Multi_Task_continual_Training" --Group_num 1 --Corpus_list "DDI" "CPR" "Twi_ADE" "ADE"  --All_data  >/dev/null 2>&1 &
 
 python run.py --ID 32 --GPU 0 --Training_way "Continual_Training" --Entity_Prep_Way "entitiy_type_marker" --Group_num 1 --Corpus_list "Combine_ADE" "DDI" "CPR" --Only_relation --All_data
+
+
+
+
+python run_with_memory_samples.py --ID 1 --GPU 0 --Training_way "Continual_Training" --Entity_Prep_Way "entitiy_type_marker" --Corpus_list "Combine_ADE" "DDI" "CPR" --Only_relation --All_data
