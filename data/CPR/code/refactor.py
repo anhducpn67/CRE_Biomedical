3305eb0a5c3887fde155b2b981b55cbf5d8d436e
Nguyen Tran Anh Duc <ducnta3@ghtk.co>
2023-05-01 17:36:23 +0700

    Add code refactor for data

---
+++ data/CPR/code/refactor.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+task = "CPR"
+
+
+def refactor(path_in, path_out):
+    f = open(path_in, "r").readlines()
+    f_out = open(path_out, "w")
+    for line in f:
+        obj = eval(line)
+        del obj["entity_span"]
+        del obj["entity_span_and_type"]
+        del obj["joint_entity_type_Drug"]
+        del obj["joint_entity_type_Gene"]
+        del obj["sampled_entity_span"]
+        f_out.write(json.dumps(obj))
+        f_out.write("\n")
+
+
+files_path = [(f"../BIOES/base/{task}_{i}_base_model_data.json", f"../BIOES/base/new_{task}_{i}_base_model_data.json")
+              for i in ["train", "valid", "test"]]
+files_path += [
+    (f"../BIOES/base/test/{task}_{i}_base_model_data.json", f"../BIOES/base/test/new_{task}_{i}_base_model_data.json")
+    for i in ["train", "valid", "test"]]
+
+for path_in, path_out in files_path:
+    refactor(path_in, path_out)
+
+[os.rename(f"../BIOES/base/{task}_{i}_base_model_data.json", f"../BIOES/base/old_{task}_{i}_base_model_data.json") for i
+ in ["train", "valid", "test"]]
+[os.rename(f"../BIOES/base/new_{task}_{i}_base_model_data.json", f"../BIOES/base/{task}_{i}_base_model_data.json") for i
+ in ["train", "valid", "test"]]
+
+[os.rename(f"../BIOES/base/test/{task}_{i}_base_model_data.json",
+           f"../BIOES/base/test/old_{task}_{i}_base_model_data.json") for i in ["train", "valid", "test"]]
+[os.rename(f"../BIOES/base/test/new_{task}_{i}_base_model_data.json",
+           f"../BIOES/base/test/{task}_{i}_base_model_data.json") for i in ["train", "valid", "test"]]
